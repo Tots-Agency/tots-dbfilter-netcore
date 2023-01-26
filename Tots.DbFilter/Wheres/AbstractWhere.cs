a435b23abe775b39f283c87979ca9909da7c5c69
Matias Camiletti <matias.camiletti@gmail.com>
2023-01-26 09:58:03 -0300

    Add where abstract and equal.

---
+++ Tots.DbFilter/Wheres/AbstractWhere.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using Tots.DbFilter.Extensions;
+
+namespace Tots.DbFilter.Wheres
+{
+	abstract public class AbstractWhere
+	{
+        public static string TYPE_LIKES = "likes";
+        public static string TYPE_EQUAL = "equal";
+
+        protected string _type = "";
+        protected string _key = "";
+        protected dynamic? _value;
+
+        abstract public Expression<Func<T, bool>> ExecutePredicate<T>();
+
+        public string CleanKey(string key)
+        {
+            return key.FirstCharToUpper().Replace(" ", "").Replace(";", "");
+        }
+
+        public bool IsSameKey(string key)
+        {
+            if (this._key == key)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetTypeWhere()
+        {
+            return this._type;
+        }
+
+        public string GetKey()
+        {
+            return this._key;
+        }
+
+        public object? GetValue()
+        {
+            return this._value;
+        }
+    }
+}
+
---
+++ Tots.DbFilter/Wheres/EqualWhere.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using Tots.DbFilter.Entities;
+using Tots.DbFilter.Extensions;
+
+namespace Tots.DbFilter.Wheres
+{
+	public class EqualWhere: AbstractWhere
+	{
+        public EqualWhere(WhereEntity data)
+        {
+            this._type = AbstractWhere.TYPE_EQUAL;
+            this._key = data.Key ?? "";
+            this._value = data.Value ?? "";
+        }
+
+        public override Expression<Func<T, bool>> ExecutePredicate<T>()
+        {
+            string key = this.CleanKey(this.GetKey());
+            var value = this.GetValue();
+
+            if (value == null)
+            {
+                return PredicateBuilderExtension.Equal<T>(key, "");
+            }
+
+            int valueInt;
+            if (Int32.TryParse(value.ToString(), out valueInt))
+            {
+                return PredicateBuilderExtension.Equal<T>(key, valueInt);
+            }
+
+            return PredicateBuilderExtension.Equal<T>(key, value.ToString()!);
+        }
+    }
+}
+
