7e55ac3600eecd84266812b14321df5de09f36c1
Matias Camiletti <matias.camiletti@gmail.com>
2023-01-26 10:07:02 -0300

    Add list request.

---
+++ Tots.DbFilter/Requests/TotsDbListRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using Tots.DbFilter.Responses;
+
+namespace Tots.DbFilter.Requests
+{
+	public class TotsDbListRequest<T>: IRequest<TotsDbListResponse<T>>
+	{
+        public string[]? With { get; set; }
+        //public int? Limit { get; set; }
+        public int? Page { get; set; }
+        public int? PerPage { get; set; }
+        public string? Filters { get; set; }
+        public string? FiltersString { get; set; }
+    }
+}
+
